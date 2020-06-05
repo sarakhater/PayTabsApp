5df0df6b27a83a02909399d1a7c2a2e9a358e56e
Sara Khater <sara.khater@future-group.com>
2020-06-05 16:23:25 +0200

    Add indicator and enhancment

--- PayTabsAssignments/PaymentResultViewController.swift
+++ PayTabsAssignments/PaymentResultViewController.swift
@@ -16,9 +16,7 @@ class PaymentResultViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
-    
     var resultString = ""
-    var resultCode = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,28 +25,10 @@ class PaymentResultViewController: UIViewController {
         bigView.layer.cornerRadius = 15;
         bigView.layer.borderWidth = 1;
         bigView.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-
-       
     }
     
     func setData(){
         self.resultLabel.text = resultString;
-               
-//               if resultCode == 100 {
-//                   self.image.image = UIImage(named: "successful-icon")
-//               }else{
-//                     self.image.image = UIImage(named: "unsuccessful-icon")
-//               }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- PayTabsAssignments/ViewController.swift
+++ PayTabsAssignments/ViewController.swift
@@ -6,8 +6,8 @@
 //
 //For test CheckOut with below data
 /*Name on Card:  John Doe
-Card Number:  4000 0000 0000 0051
-Expiry:  02/22  CVV:  111*/
+ Card Number:  4000 0000 0000 0051
+ Expiry:  02/22  CVV:  111*/
 
 
 
@@ -22,94 +22,100 @@ class ViewController: UIViewController {
     var resultController = PaymentResultViewController()
     
     var secretKey = "bzRMnRWyl7d80ouETHBo7zKTFUI8NGBYfsLCkHaFOoOPNitG0557EP1kJJsxp4vFxhiRZswORkDED7lDcqKRgLb0vExUI3zfkR7j"
-
-
+    
+    var activityIndicator = UIActivityIndicatorView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-       
+        
+        
     }
-
-
+    
+    
     //MARK:- Actions
     @IBAction func PayNowClicked(_ sender: UIButton) {
-          showPaymentScreen();
+        showPaymentScreen();
         
     }
     
     func showPaymentScreen(){
         let bundle = Bundle(url: Bundle.main.url(forResource: "Resources", withExtension: "bundle")!)
-                       self.initialSetupViewController = PTFWInitialSetupViewController.init(
-                                     bundle: bundle,
-                                     andWithViewFrame: self.view.frame,
-                                     andWithAmount: 5.0,
-                                     andWithCustomerTitle: "Pay Now With PayTabs",
-                                     andWithCurrencyCode: "EGP",
-                                     andWithTaxAmount: 0.0,
-                                     andWithSDKLanguage: "en",
-                                     andWithShippingAddress: "Cairo",
-                                     andWithShippingCity: "Cairo",
-                                     andWithShippingCountry: "BHR",
-                                     andWithShippingState: "Cairo",
-                                     andWithShippingZIPCode: "123456",
-                                     andWithBillingAddress: "Cairo",
-                                     andWithBillingCity: "Cairo",
-                                     andWithBillingCountry: "BHR",
-                                     andWithBillingState: "Cairo",
-                                     andWithBillingZIPCode: "12345",
-                                     andWithOrderID: "100",
-                                     andWithPhoneNumber: "00201028284349",
-                                     andWithCustomerEmail: "rhegazy@paytabs.com",
-                                     andIsTokenization:false,
-                                     andIsPreAuth: false,
-                                     andWithMerchantEmail: "sarakhater2014@gmail.com",
-                                     andWithMerchantSecretKey: secretKey,
-                                     andWithAssigneeCode: "SDK",
-                                     andWithThemeColor:UIColor.red,
-                                     andIsThemeColorLight: false)
-                          
-                          self.initialSetupViewController.didReceiveBackButtonCallback = {
-                              
-                          }
-                          
-                          self.initialSetupViewController.didStartPreparePaymentPage = {
-                              // Start Prepare Payment Page
-                              // Show loading indicator
-                          }
-                          self.initialSetupViewController.didFinishPreparePaymentPage = {
-                              // Finish Prepare Payment Page
-                              // Stop loading indicator
-                          }
-                          
-                          self.initialSetupViewController.didReceiveFinishTransactionCallback = {(responseCode, result, transactionID, tokenizedCustomerEmail, tokenizedCustomerPassword, token, transactionState) in
-                              print("Response Code: \(responseCode)")
-                              print("Response Result: \(result)")
-                              
-                              // In Case you are using tokenization
-                              print("Tokenization Cutomer Email: \(tokenizedCustomerEmail)");
-                              print("Tokenization Customer Password: \(tokenizedCustomerPassword)");
-                              print("TOkenization Token: \(token)");
-                            
-                           
-                            
-                            
-                            let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                            self.resultController = storyBoard.instantiateViewController(withIdentifier: "PaymentResultViewController") as! PaymentResultViewController
-                            
-                            
-                            
-                            self.resultController.resultCode = Int(responseCode);
-                                                       self.resultController.resultString = result
-                            
-                        self.navigationController?.pushViewController(self.resultController, animated: true)
-                            //self.present(self.resultController, animated: true, completion: nil)
-                          }
-
-                          self.view.addSubview(initialSetupViewController.view)
-                          self.addChild(initialSetupViewController)
-                          
-                          initialSetupViewController.didMove(toParent: self)
+        self.initialSetupViewController = PTFWInitialSetupViewController.init(
+            bundle: bundle,
+            andWithViewFrame: self.view.frame,
+            andWithAmount: 5.0,
+            andWithCustomerTitle: "Pay Now With PayTabs",
+            andWithCurrencyCode: "EGP",
+            andWithTaxAmount: 0.0,
+            andWithSDKLanguage: "en",
+            andWithShippingAddress: "Cairo",
+            andWithShippingCity: "Cairo",
+            andWithShippingCountry: "BHR",
+            andWithShippingState: "Cairo",
+            andWithShippingZIPCode: "123456",
+            andWithBillingAddress: "Cairo",
+            andWithBillingCity: "Cairo",
+            andWithBillingCountry: "BHR",
+            andWithBillingState: "Cairo",
+            andWithBillingZIPCode: "12345",
+            andWithOrderID: "100",
+            andWithPhoneNumber: "00201028284349",
+            andWithCustomerEmail: "rhegazy@paytabs.com",
+            andIsTokenization:false,
+            andIsPreAuth: false,
+            andWithMerchantEmail: "sarakhater2014@gmail.com",
+            andWithMerchantSecretKey: secretKey,
+            andWithAssigneeCode: "SDK",
+            andWithThemeColor:UIColor.red,
+            andIsThemeColorLight: false)
+        
+        self.initialSetupViewController.didReceiveBackButtonCallback = {
+            
+        }
+        
+        self.initialSetupViewController.didStartPreparePaymentPage = {
+            // Start Prepare Payment Page
+            // Show loading indicator
+            self.activityIndicator.startAnimating()
+        }
+        self.initialSetupViewController.didFinishPreparePaymentPage = {
+            // Finish Prepare Payment Page
+            // Stop loading indicator
+            self.activityIndicator.stopAnimating()
+        }
+        
+        self.initialSetupViewController.didReceiveFinishTransactionCallback = {(responseCode, result, transactionID, tokenizedCustomerEmail, tokenizedCustomerPassword, token, transactionState) in
+            print("Response Code: \(responseCode)")
+            print("Response Result: \(result)")
+            
+            // In Case you are using tokenization
+            print("Tokenization Cutomer Email: \(tokenizedCustomerEmail)");
+            print("Tokenization Customer Password: \(tokenizedCustomerPassword)");
+            print("TOkenization Token: \(token)");
+            
+            
+            self.presenResultController(result : result);
+            
+            
+            
+        }
+        
+        self.view.addSubview(initialSetupViewController.view)
+        self.addChild(initialSetupViewController)
+        
+        initialSetupViewController.didMove(toParent: self)
     }
     
+    
+    func presenResultController(result : String){
+        
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        self.resultController = storyBoard.instantiateViewController(withIdentifier: "PaymentResultViewController") as! PaymentResultViewController;
+        self.resultController.resultString = result
+        
+        self.navigationController?.pushViewController(self.resultController, animated: true)
+        
+    }
 }
 
